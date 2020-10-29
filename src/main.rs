c8f78c55a5bb3538974b390b6b212a9e4916377c
nikhil.jain0987@gmail.com <nikhil.jain0987@gmail.com>
2020-10-29 01:12:30 -0400

    working on add instr

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,7 @@ enum Register {
     COND,
 }
 
-const REGISTER_COUNT:usize = 11;
-
+const REGISTER_COUNT:usize = instruction::REGISTER_COUNT;
 
 enum Opcode {
     Branch = 0 as isize,
@@ -72,7 +71,7 @@ fn main() {
         match opcode {
             // why can't an enum just be cast to a u16, am i doing something wrong
             _ if opcode == Opcode::Branch as u16 => instruction::branch_instr(),
-            _ if opcode == Opcode::Add as u16 => instruction::add_instr(),
+            _ if opcode == Opcode::Add as u16 => add_instr(instruction, &mut registers),
             _ if opcode == Opcode::Load as u16 => instruction::load_instr(),
             _ if opcode == Opcode::Store as u16 => instruction::store_instr(),
             _ if opcode == Opcode::JumpRegister as u16 => instruction::jumpreg_instr(),
@@ -108,3 +107,92 @@ fn load_program(path : &str) -> Vec<u16> {
 fn mem_read(addr : u16) -> u16 {
     return 0x130;
 }
+
+/// Module for implementation of the lc3 instructions 
+
+/// Sign extension utility function
+fn sign_extend(num:u16, count:u16) -> u16 {
+    // if the msb is 1, sign extend with 1s
+    if (num >> (count - 1)) & 1 != 0 {
+        return num | (0xffff << count);
+    }
+    num
+}
+
+/// Update condition flags any time a value is written to a register 
+fn update_flags(reg: usize, registers: &mut [u16; REGISTER_COUNT]) {
+    if registers[reg] == 0 {
+        registers[Register::COND as usize] = ConditionFlags::Zero as u16;
+    }
+    else if registers[reg] >> 15 == 1 { // MSB = 1 means negative in 2s complement
+        registers[Register::COND as usize] = ConditionFlags::Neg as u16;
+    }
+    else {
+        registers[Register::COND as usize] = ConditionFlags::Pos as u16;
+    }
+}
+
+/// Handles ADD DR, SR1, SR2 and ADD DR, SR1, imm5
+pub fn add_instr(instr: u16, registers: &mut [u16; REGISTER_COUNT]) {
+    let dr:usize = ((instr >> 9) & 0x7) as usize;
+    let sr1:usize = ((instr >> 6) & 0x7) as usize;
+    let imm_flag:u16 = (instr >> 5) & 0x1;
+
+    if imm_flag == 1 {
+        // add immediate
+        let imm:u16 = sign_extend(instr & 0x1F, 5);
+        registers[dr] = registers[sr1] + imm
+    }
+    else {
+        let sr2:usize = (instr & 0x7) as usize;
+        registers[dr] = registers[sr1] + registers[sr2];
+    }
+    
+    update_flags(dr, registers);
+    
+}
+
+pub fn and_instr() {
+
+}
+pub fn not_instr() {
+
+}
+pub fn branch_instr() {
+
+}
+pub fn jump_instr() {
+
+}
+pub fn jumpreg_instr() {
+
+}
+pub fn load_instr() {
+
+}
+
+pub fn store_instr() {
+
+}
+
+pub fn loadreg_instr() {
+
+}
+
+pub fn loadindr_instr() {
+
+}
+pub fn lea_instr() {
+
+}
+pub fn trap_instr() {
+
+}
+
+pub fn storeg_instr() {
+
+}
+pub fn storeindr_instr() {
+
+}
+
